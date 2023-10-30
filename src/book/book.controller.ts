d64c670a9ca05a2f7d253966b332dcd0eac14896
Sasikant Biswal <biswalsashikant@gmail.com>
2023-10-31 00:43:14 +0530

    Added User module and protected routes

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,21 +1,35 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schema';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('books')
 export class BookController {
   constructor(private bookService: BookService) {}
 
   @Get()
-  async getAllBooks(): Promise<Book[]> {
-    return this.bookService.findAllBooks();
+  async getAllBooks(@Query() query: ExpressQuery): Promise<Book[]> {
+    return this.bookService.findAllBooks(query);
   }
 
   @Post()
-  async createBook(@Body() book: CreateBookDto): Promise<Book> {
-    return this.bookService.createBook(book);
+  @UseGuards(AuthGuard())
+  async createBook(@Body() book: CreateBookDto, @Req() req): Promise<Book> {
+    return this.bookService.createBook(book, req.user);
   }
 
   @Get(':id')
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,7 +1,13 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 import { Book } from './schemas/book.schema';
+import { Query } from 'express-serve-static-core';
+import { User } from 'src/auth/schemas/user.schema';
 
 @Injectable()
 export class BookService {
@@ -10,17 +16,40 @@ export class BookService {
     private bookModel: mongoose.Model<Book>,
   ) {}
 
-  async findAllBooks(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async findAllBooks(query: Query): Promise<Book[]> {
+    const resPerPage = 2;
+    const currentPage = Number(query.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
+
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i', // i means case insensitive
+          },
+        }
+      : {};
+    const books = await this.bookModel
+      .find({ ...keyword })
+      .limit(resPerPage)
+      .skip(skip);
     return books;
   }
 
-  async createBook(book: Book): Promise<Book> {
-    const res = await this.bookModel.create(book);
+  async createBook(book: Book, user: User): Promise<Book> {
+    const data = Object.assign(book, { user: user._id });
+
+    const res = await this.bookModel.create(data);
     return res;
   }
 
   async findBook(id: string): Promise<Book> {
+    const isValidId = mongoose.isValidObjectId(id);
+
+    if (!isValidId) {
+      throw new BadRequestException(`Please enter correct id.`);
+    }
+
     const book = await this.bookModel.findById(id);
 
     if (!book) {
